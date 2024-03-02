bde9dc4f450d5b968d67ab7ccc2ffaf71d95c7f9
marsalan <marsalan@hhaexhange.com>
2024-03-02 16:24:13 +0530

    files path

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,27 @@
 import express from "express";
 import cors from "cors";
+import { generate } from "./utils";
+import simpleGit from "simple-git";
+import path from "path";
+import { getAllFiles } from "./file";
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
-app.post("/deploy", (req, res) => {
+app.post("/deploy", async (req, res) => {
     const repoUrl = req.body.repoUrl;
-    console.log(repoUrl);
-    res.json({});
+    const id = generate();
+    
+    await simpleGit().clone(repoUrl, path.join(__dirname, `output/${id}`));
+    
+    const files = getAllFiles(path.join(__dirname, `output/${id}`));
+
+    console.log(files);
+
+    res.json({
+        id: id
+    });
 })
 
 app.listen(3000, () => {
